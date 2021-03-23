0a1ee132c186f01a8cb7dfefc3de9f67fc818d91
mistic10 <hypnolog@yahoo.fr>
2021-03-23 10:53:04 +0100

    annulation corection

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('../sw.js', { scope: '/' })
+    navigator.serviceWorker.register('/sw.js', { scope: '/' })
     .then(function(reg){
         if(reg.installing) {
             console.log('Service worker installing')
